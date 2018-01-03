5b8f01adc3505183d5716211756cd5cbc95804cb
vatsal <30840527+vatsalkul@users.noreply.github.com>
2018-01-03 16:34:03 +0530

    Commenting About View Controller

--- Powerup/AboutViewController.swift
+++ Powerup/AboutViewController.swift
@@ -4,6 +4,7 @@ class AboutViewController: UIViewController {
     
     // MARK: Action
     @IBAction func homeButtonTouched(_ sender: UIButton) {
+        // Showing about section of app.
         dismiss(animated: true, completion: nil)
     }
 }
